2783a7b0eec62d9f2c84c69ac053504b0d7880b6
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-08-28 16:11:06 -0500

    feat: Añadir soporte para selección de esquema de color en el tema personalizado

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import { CssVarsProvider, extendTheme } from "@mui/joy";
+
+const customTheme = extendTheme({
+    colorSchemeSelector: "media",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return (
+        <CssVarsProvider theme={customTheme} defaultMode="system">
+            <Component {...pageProps} />
+        </CssVarsProvider>
+    );
 }
