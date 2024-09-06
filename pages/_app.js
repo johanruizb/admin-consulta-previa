bdef63ee9d4bbb1b7bf9e5e0fed08fe01d44e3ff
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-05 22:03:04 -0500

    Añadir disableTransitionOnChange a CssVarsProvider en _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,11 @@ const customTheme = extendTheme({
 
 export default function App({ Component, pageProps }) {
     return (
-        <CssVarsProvider theme={customTheme} defaultMode="system">
+        <CssVarsProvider
+            theme={customTheme}
+            defaultMode="system"
+            disableTransitionOnChange
+        >
             <Component {...pageProps} />
         </CssVarsProvider>
     );
