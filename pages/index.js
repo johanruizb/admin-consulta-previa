bc071571770930d1bc56249769ca6e55722e8c66
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-05 22:03:11 -0500

    feat: Añadir página de inicio con barra de navegación y tabla de pedidos

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,75 @@
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import Box from "@mui/joy/Box";
+import Breadcrumbs from "@mui/joy/Breadcrumbs";
+import Button from "@mui/joy/Button";
+import Link from "@mui/joy/Link";
+import Typography from "@mui/joy/Typography";
+
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
+import DownloadRoundedIcon from "@mui/icons-material/DownloadRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+
+import Layout from "@/components/Home/Layout";
+import OrderList from "@/components/Home/OrderList";
+import OrderTable from "@/components/Home/OrderTable";
 
 export default function Home() {
-    const router = useRouter();
-
-    useEffect(() => {
-        router.push("/login");
-    }, [router]);
-
-    return;
+    return (
+        <Layout>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+                <Breadcrumbs
+                    size="sm"
+                    aria-label="breadcrumbs"
+                    separator={<ChevronRightRoundedIcon fontSize="sm" />}
+                    sx={{ pl: 0 }}
+                >
+                    <Link
+                        underline="none"
+                        color="neutral"
+                        href="#some-link"
+                        aria-label="Home"
+                    >
+                        <HomeRoundedIcon />
+                    </Link>
+                    <Link
+                        underline="hover"
+                        color="neutral"
+                        href="#some-link"
+                        sx={{ fontSize: 12, fontWeight: 500 }}
+                    >
+                        Dashboard
+                    </Link>
+                    <Typography
+                        color="primary"
+                        sx={{ fontWeight: 500, fontSize: 12 }}
+                    >
+                        Orders
+                    </Typography>
+                </Breadcrumbs>
+            </Box>
+            <Box
+                sx={{
+                    display: "flex",
+                    mb: 1,
+                    gap: 1,
+                    flexDirection: { xs: "column", sm: "row" },
+                    alignItems: { xs: "start", sm: "center" },
+                    flexWrap: "wrap",
+                    justifyContent: "space-between",
+                }}
+            >
+                <Typography level="h2" component="h1">
+                    Orders
+                </Typography>
+                <Button
+                    color="primary"
+                    startDecorator={<DownloadRoundedIcon />}
+                    size="sm"
+                >
+                    Download PDF
+                </Button>
+            </Box>
+            <OrderTable />
+            <OrderList />
+        </Layout>
+    );
 }
