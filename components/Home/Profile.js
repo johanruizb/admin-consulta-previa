4cdd097e9337a17eacd15e95ea8bb198c98cedfe
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-06 21:53:45 -0500

    Actualizar manejo de errores en el inicio de sesión y agregar funcionalidad de cierre de sesión en la barra lateral

---
+++ components/Home/Profile.js
@@ -0,0 +1,80 @@
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+
+import Avatar from "@mui/joy/Avatar";
+import Box from "@mui/joy/Box";
+import IconButton from "@mui/joy/IconButton";
+import Typography from "@mui/joy/Typography";
+
+import { useRouter } from "next/router";
+
+import { useState } from "react";
+
+import useSWR from "swr";
+import fetcher from "@/components/fetcher";
+import { Skeleton } from "@mui/joy";
+
+export default function Profile() {
+    const { data, error, isLoading } = useSWR("api/user", fetcher, {
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+    });
+
+    const [logout, setLogout] = useState(false);
+    const router = useRouter();
+
+    const onLogout = () => {
+        setLogout(true);
+
+        fetch("/api/logout", {
+            method: "POST",
+        })
+            .then(() => {
+                router.reload();
+            })
+            .finally(() => {
+                setLogout(false);
+            });
+    };
+
+    return (
+        <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+            {/* <Avatar variant="outlined" size="sm" /> */}
+            <Box sx={{ minWidth: 0, flex: 1 }}>
+                <Typography level="title-sm">
+                    <Skeleton
+                        loading={isLoading}
+                        width="100%"
+                        sx={{
+                            display: "inline-block",
+                            height: "9px",
+                        }}
+                    >
+                        {data?.fullname ?? data?.username}
+                    </Skeleton>
+                </Typography>
+                <Typography level="body-xs">
+                    <Skeleton
+                        loading={isLoading}
+                        width="100%"
+                        sx={{
+                            display: "inline-block",
+                            height: "9px",
+                        }}
+                    >
+                        {data?.role}
+                    </Skeleton>
+                </Typography>
+            </Box>
+            <IconButton
+                size="sm"
+                variant="plain"
+                color="neutral"
+                onClick={onLogout}
+                loading={logout}
+            >
+                <LogoutRoundedIcon />
+            </IconButton>
+        </Box>
+    );
+}
