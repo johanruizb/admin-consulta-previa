a6d46eda35e285b1da9aaf860ab1858ad0a8bcd4
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-06 21:55:40 -0500

    Agregar página de panel con título y breadcrumbs

---
+++ pages/panel/index.js
@@ -0,0 +1,58 @@
+import Box from "@mui/joy/Box";
+import Breadcrumbs from "@mui/joy/Breadcrumbs";
+import Link from "@mui/joy/Link";
+import Typography from "@mui/joy/Typography";
+
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+
+import Layout from "@/components/Home/Layout";
+import Head from "next/head";
+
+export default function Panel() {
+    return (
+        <Layout>
+            <Head>
+                <title>Inicio - Consulta previa</title>
+            </Head>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+                <Breadcrumbs
+                    size="sm"
+                    aria-label="breadcrumbs"
+                    separator={<ChevronRightRoundedIcon fontSize="sm" />}
+                    sx={{ pl: 0 }}
+                >
+                    <Link
+                        underline="none"
+                        color="neutral"
+                        href="/"
+                        aria-label="Home"
+                    >
+                        <HomeRoundedIcon />
+                    </Link>
+                    <Typography
+                        color="primary"
+                        sx={{ fontWeight: 500, fontSize: 12 }}
+                    >
+                        Panel
+                    </Typography>
+                </Breadcrumbs>
+            </Box>
+            <Box
+                sx={{
+                    display: "flex",
+                    mb: 1,
+                    gap: 1,
+                    flexDirection: { xs: "column", sm: "row" },
+                    alignItems: { xs: "start", sm: "center" },
+                    flexWrap: "wrap",
+                    justifyContent: "space-between",
+                }}
+            >
+                <Typography level="h2" component="h1">
+                    Panel
+                </Typography>
+            </Box>
+        </Layout>
+    );
+}
