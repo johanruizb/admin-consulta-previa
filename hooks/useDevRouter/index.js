851ca47f91afd9039b3f5b53b7514798c5fe9bd7
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-06 21:55:03 -0500

    Agregar hook useDevRouter para redirigir en entorno de desarrollo

---
+++ hooks/useDevRouter/index.js
@@ -0,0 +1,14 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+function useDevRouter() {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (process.env.NODE_ENV === "production") router.push("/registros");
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+}
+
+export default useDevRouter;
