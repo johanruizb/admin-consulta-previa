4b5f0acd1695d4246a452eb47588a38b417ca1bf
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-06 21:53:28 -0500

    Actualizar manejo de errores en el inicio de sesión y agregar funcionalidad de cierre de sesión en la barra lateral

--- components/Home/ColorSchemeToggle.js
+++ components/Home/ColorSchemeToggle.js
@@ -1,45 +1,41 @@
-import IconButton from "@mui/joy/IconButton";
-import { useColorScheme } from "@mui/joy/styles";
 import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
 import LightModeIcon from "@mui/icons-material/LightMode";
+import IconButton from "@mui/joy/IconButton";
+
 import { useEffect, useState } from "react";
+
+import { useColorScheme as useJoyColorScheme } from "@mui/joy/styles";
+import { useColorScheme as useMaterialColorScheme } from "@mui/material/styles";
 
 export default function ColorSchemeToggle(props) {
     const { onClick, sx, ...other } = props;
-    const { mode, setMode } = useColorScheme();
+    const { mode, setMode: setMaterialMode } = useMaterialColorScheme();
+    const { setMode: setJoyMode } = useJoyColorScheme();
     const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
         setMounted(true);
     }, []);
 
-    if (!mounted) {
-        return (
-            <IconButton
-                size="sm"
-                variant="outlined"
-                color="neutral"
-                {...other}
-                sx={sx}
-                disabled
-            />
-        );
+    if (!mounted || !mode) {
+        // prevent server-side rendering mismatch
+        // because `mode` is undefined on the server.
+        return null;
     }
+
+    console.log("mode", mode);
+
     return (
         <IconButton
             data-screenshot="toggle-mode"
             size="sm"
             variant="outlined"
             color="neutral"
-            {...other}
-            onClick={(event) => {
-                if (mode === "light") {
-                    setMode("dark");
-                } else {
-                    setMode("light");
-                }
-                onClick?.(event);
+            onClick={() => {
+                setMaterialMode(mode === "dark" ? "light" : "dark");
+                setJoyMode(mode === "dark" ? "light" : "dark");
             }}
+            {...other}
             sx={[
                 mode === "dark"
                     ? { "& > *:first-of-type": { display: "none" } }
