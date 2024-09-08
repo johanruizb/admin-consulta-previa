f312b95b622f207a106c5bcc51f9df9782ebf9b5
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-07 19:31:46 -0500

    Refactorizar componente Panel en la página de inicio para agregar estadísticas de personas registradas y validadas

--- pages/panel/index.js
+++ pages/panel/index.js
@@ -1,3 +1,4 @@
+import { Card, CardContent, CircularProgress, Skeleton } from "@mui/joy";
 import Box from "@mui/joy/Box";
 import Breadcrumbs from "@mui/joy/Breadcrumbs";
 import Link from "@mui/joy/Link";
@@ -6,10 +7,47 @@ import Typography from "@mui/joy/Typography";
 import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 
+import { Stack } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+
 import Layout from "@/components/Home/Layout";
+
 import Head from "next/head";
 
+import dayjs from "dayjs";
+import "dayjs/locale/es";
+
+import { useEffect, useState } from "react";
+
+import { formatNumber } from "@/components/utils";
+import fetcher from "@/components/fetcher";
+
+import useSWR from "swr";
+
+dayjs.locale("es");
+
 export default function Panel() {
+    const { data, isLoading } = useSWR("api/usuarios/estadisticas", fetcher);
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return (
+            <Stack
+                justifyContent="center"
+                alignContent="center"
+                alignItems="center"
+                width="100%"
+                height="100%"
+            >
+                <CircularProgress />
+            </Stack>
+        );
+    }
+
     return (
         <Layout>
             <Head>
@@ -50,9 +88,101 @@ export default function Panel() {
                 }}
             >
                 <Typography level="h2" component="h1">
-                    Panel
+                    Panel de estadísticas
                 </Typography>
             </Box>
+            <Grid container spacing={1.25}>
+                <Grid size={{ xs: 12, md: 4 }}>
+                    <Card variant="plain">
+                        <CardContent>
+                            <Typography level="title-lg">
+                                Personas registradas
+                            </Typography>
+                            <Stack
+                                direction="row"
+                                alignItems="center"
+                                justifyContent="space-between"
+                                spacing={1.25}
+                            >
+                                <Typography level="body-md">Hoy</Typography>
+                                <Typography level="h2">
+                                    {isLoading ? (
+                                        <CircularProgress size="sm" />
+                                    ) : (
+                                        formatNumber(data.today)
+                                    )}
+                                </Typography>
+                            </Stack>
+                            <Stack
+                                direction="row"
+                                alignItems="center"
+                                justifyContent="space-between"
+                                spacing={1.25}
+                            >
+                                <Typography level="body-md">
+                                    Total, desde el inicio
+                                </Typography>
+                                <Typography level="h2">
+                                    {isLoading ? (
+                                        <CircularProgress size="sm" />
+                                    ) : (
+                                        formatNumber(data.total)
+                                    )}
+                                </Typography>
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid size={{ xs: 12, md: 4 }}>
+                    <Card
+                        variant="plain"
+                        sx={{
+                            width: "100%",
+                            height: "100%",
+                        }}
+                    >
+                        <CardContent>
+                            <Typography level="title-lg">
+                                Personas validadas (
+                                <Skeleton
+                                    loading={isLoading}
+                                    variant="inline"
+                                    width="24px"
+                                    height="24px"
+                                >
+                                    {data?.percentage}
+                                </Skeleton>
+                                %)
+                            </Typography>
+                            <Stack flex={1} spacing={0} justifyContent="center">
+                                <Stack
+                                    direction="row"
+                                    // alignItems="center"
+                                    alignItems="baseline"
+                                    justifyContent="center"
+                                    spacing={1.25}
+                                >
+                                    <Typography level="h2">
+                                        {isLoading ? (
+                                            <CircularProgress size="sm" />
+                                        ) : (
+                                            formatNumber(data.validated)
+                                        )}
+                                    </Typography>
+                                    <Typography level="body-md">de</Typography>
+                                    <Typography level="h2">
+                                        {isLoading ? (
+                                            <CircularProgress size="sm" />
+                                        ) : (
+                                            formatNumber(data.total)
+                                        )}
+                                    </Typography>
+                                </Stack>
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
         </Layout>
     );
 }
