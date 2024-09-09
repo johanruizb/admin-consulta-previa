fdd2638e42ea92c24ca639ae154ef93b505b3ac3
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-09 15:08:02 -0500

    Refactorizar componente [id].js para agregar soporte de visualización de registros con formulario personalizado

--- pages/registros/[id].js
+++ pages/registros/[id].js
@@ -1,54 +1,10 @@
-import Button from "@mui/joy/Button";
-import DialogContent from "@mui/joy/DialogContent";
-import DialogTitle from "@mui/joy/DialogTitle";
-import FormControl from "@mui/joy/FormControl";
-import FormLabel from "@mui/joy/FormLabel";
-import Input from "@mui/joy/Input";
-import Modal from "@mui/joy/Modal";
-import ModalDialog from "@mui/joy/ModalDialog";
-import Stack from "@mui/joy/Stack";
+import CustomModal from "@/components/Modal/CustomModal";
+import Registros from ".";
 
-import { useRouter } from "next/navigation";
-
-import * as React from "react";
-
-export default function BasicModalDialog({ props }) {
-    const router = useRouter();
-
-    const onClose = () => {
-        router.back();
-    };
-
-    console.log(props);
-
+export default function View() {
     return (
-        <React.Fragment>
-            <Modal open onClose={onClose}>
-                <ModalDialog>
-                    <DialogTitle>Create new project</DialogTitle>
-                    <DialogContent>
-                        Fill in the information of the project.
-                    </DialogContent>
-                    <form
-                        onSubmit={(event) => {
-                            event.preventDefault();
-                            setOpen(false);
-                        }}
-                    >
-                        <Stack spacing={2}>
-                            <FormControl>
-                                <FormLabel>Name</FormLabel>
-                                <Input autoFocus required />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>Description</FormLabel>
-                                <Input required />
-                            </FormControl>
-                            <Button type="submit">Submit</Button>
-                        </Stack>
-                    </form>
-                </ModalDialog>
-            </Modal>
-        </React.Fragment>
+        <Registros>
+            <CustomModal />
+        </Registros>
     );
 }
