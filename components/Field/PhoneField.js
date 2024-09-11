b725dd28338434bbc829885cb44689d0447cbdf0
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-10 22:25:28 -0500

    Refactorizar componente PhoneField para reemplazar importaciones de @mui/joy por @mui/material y agregar dependencia react-hook-form

---
+++ components/Field/PhoneField.js
@@ -0,0 +1,69 @@
+import FormControl from "@mui/joy/FormControl";
+import FormHelperText from "@mui/joy/FormHelperText";
+import FormLabel from "@mui/joy/FormLabel";
+import Input from "@mui/joy/Input";
+import { Controller, useFormContext } from "react-hook-form";
+
+import { forwardRef } from "react";
+import { IMaskInput } from "react-imask";
+
+const PhoneMask = forwardRef(function PhoneMask(props, ref) {
+    const { onChange, ...other } = props;
+    return (
+        <IMaskInput
+            {...other}
+            // mask={`+${COLOMBIA.phonecode} #00 000 0000`}
+            mask={`#00 000 0000`}
+            definitions={{
+                "#": /[1-9]/,
+            }}
+            inputRef={ref}
+            onAccept={(value) =>
+                onChange({ target: { name: props.name, value } })
+            }
+            overwrite
+        />
+    );
+});
+
+export default function PhoneField({ inputProps }) {
+    const { control } = useFormContext();
+
+    const {
+        controller: controllerProps,
+        field: { InputProps, onChange: onChangeField, ...fieldProps },
+    } = inputProps;
+
+    return (
+        <Controller
+            control={control}
+            render={({ field, fieldState }) => {
+                const { error } = fieldState;
+                const { onChange: onChangeController } = field;
+
+                return (
+                    <FormControl
+                        error={Boolean(error?.type || error?.types)}
+                        required={controllerProps.rules?.required?.value}
+                    >
+                        <FormLabel>{fieldProps.label}</FormLabel>
+                        <Input
+                            {...field}
+                            onChange={(e) => {
+                                onChangeField?.(e, onChangeController) ||
+                                    onChangeController(e);
+                            }}
+                            slotProps={{
+                                input: {
+                                    component: PhoneMask,
+                                },
+                            }}
+                        />
+                        <FormHelperText>{error?.message ?? " "}</FormHelperText>
+                    </FormControl>
+                );
+            }}
+            {...controllerProps}
+        />
+    );
+}
