8c552390f225092f12335e2e9e061a2c99cec840
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-10 22:19:53 -0500

    Refactorizar componente Alert para reemplazar importaciones de @mui/joy por @mui/material y eliminar código comentado

---
+++ components/Alert/index.js
@@ -0,0 +1,77 @@
+"use client";
+import Alert from "@mui/joy/Alert";
+
+import Close from "@mui/icons-material/Close";
+
+import { IconButton } from "@mui/joy";
+import { useEffect, useRef, useState } from "react";
+import { useSessionStorage } from "@uidotdev/usehooks";
+
+export default function CustomAlert({ autoHideDuration = 6000 }) {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    return mounted ? <Custom autoHideDuration={autoHideDuration} /> : null;
+}
+
+function Custom({ autoHideDuration = 6000 }) {
+    const [config, saveConfig] = useSessionStorage("CustomAlert", {
+        open: false,
+        variant: "solid",
+        color: "success",
+        content: "",
+    });
+
+    const interval = useRef(null);
+
+    const onClose = () => {
+        saveConfig({ ...config, open: false });
+    };
+
+    useEffect(() => {
+        if (config.open) {
+            interval.current = setTimeout(() => {
+                onClose();
+            }, autoHideDuration);
+        }
+
+        return () => {
+            clearInterval(interval.current);
+        };
+    }, [config.open]);
+
+    return (
+        config.open && (
+            <Alert
+                size="lg"
+                variant={config.variant}
+                color={config.color}
+                sx={{
+                    position: "absolute",
+                    // top: 0,
+                    // left: 0,
+                    right: "10px",
+                    bottom: "10px",
+                    zIndex: 9999,
+                }}
+                endDecorator={
+                    <IconButton
+                        onClick={onClose}
+                        variant="plain"
+                        sx={{
+                            "--IconButton-size": "32px",
+                            transform: "translate(0.5rem, -0.5rem)",
+                        }}
+                    >
+                        <Close />
+                    </IconButton>
+                }
+            >
+                {config.content}
+            </Alert>
+        )
+    );
+}
