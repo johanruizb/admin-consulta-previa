9620270d3b79116df37f6ff75ca9e7d1708397fa
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-10 22:19:59 -0500

    Refactorizar componente AsyncSelect para reemplazar importaciones de @mui/joy por @mui/material y agregar dependencia react-hook-form

---
+++ components/Field/AsyncSelect.js
@@ -0,0 +1,95 @@
+import ReportIcon from "@mui/icons-material/Report";
+
+import { CircularProgress, Select as JoySelect, Option } from "@mui/joy";
+import FormControl from "@mui/joy/FormControl";
+import FormHelperText from "@mui/joy/FormHelperText";
+import FormLabel from "@mui/joy/FormLabel";
+
+import { Controller, useFormContext } from "react-hook-form";
+
+import useSWRImmutable from "swr/immutable";
+
+import fetcher from "../fetcher";
+import { getURL } from "../utils";
+
+export default function SelectWrapper({ inputProps }) {
+    const { url, ...otherProps } = inputProps;
+    const { data, error, isLoading } = useSWRImmutable(getURL(url), fetcher);
+
+    const {
+        controller: controllerProps,
+        field: { ...fieldProps },
+    } = inputProps;
+
+    return isLoading || error ? (
+        <FormControl
+            error={error}
+            required={controllerProps.rules?.required?.value}
+        >
+            <FormLabel>{fieldProps.label}</FormLabel>
+            <JoySelect
+                endDecorator={
+                    isLoading ? (
+                        <CircularProgress size="sm" />
+                    ) : (
+                        error && <ReportIcon color="danger" />
+                    )
+                }
+            />
+            <FormHelperText>
+                {error ? "No se han podido recuperar las opciones" : " "}
+            </FormHelperText>
+        </FormControl>
+    ) : (
+        <AsyncSelect
+            inputProps={{
+                ...otherProps,
+                field: { ...otherProps.field, options: data },
+            }}
+        />
+    );
+}
+
+function AsyncSelect({ inputProps }) {
+    const { control } = useFormContext();
+
+    const {
+        controller: controllerProps,
+        field: { InputProps, ...fieldProps },
+    } = inputProps;
+
+    return (
+        <Controller
+            control={control}
+            render={({ field, fieldState: { error } }) => {
+                console.log("field.value", field.value);
+
+                return (
+                    <FormControl
+                        error={Boolean(error?.type || error?.types)}
+                        required={controllerProps.rules?.required?.value}
+                    >
+                        <FormLabel>{fieldProps.label}</FormLabel>
+                        <JoySelect
+                            {...field}
+                            value={parseInt(field.value) || ""}
+                            onChange={(e, value) => field.onChange(value)}
+                        >
+                            {fieldProps.options.map((option) => (
+                                <Option
+                                    key={parseInt(option.value)}
+                                    value={parseInt(option.value)}
+                                >
+                                    {option.label}
+                                </Option>
+                            ))}
+                        </JoySelect>
+
+                        <FormHelperText>{error?.message ?? " "}</FormHelperText>
+                    </FormControl>
+                );
+            }}
+            {...controllerProps}
+        />
+    );
+}
