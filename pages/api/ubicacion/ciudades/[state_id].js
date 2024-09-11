3949dcf907b61d9003deaca81e2b9c160b4c394b
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-10 22:19:48 -0500

    Agregar rutas API para obtener países, estados y ciudades

---
+++ pages/api/ubicacion/ciudades/[state_id].js
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+/**
+ * Handler para la ruta API de Next.js que maneja la obtención de las ciudades de un estado.
+ *
+ * @param {NextApiRequest} req - El objeto de solicitud HTTP.
+ * @param {NextApiResponse} res - El objeto de respuesta HTTP.
+ *
+ * @returns {Promise<void>} - Una promesa que se resuelve cuando la operación de inicio de sesión se completa.
+ **/
+export default async function handler(req, res) {
+    const state_id = parseInt(req.query.state_id);
+
+    if (isNaN(state_id)) res.status(200).end();
+    else {
+        const response = await fetch(
+            process.env.NEXT_PUBLIC_BASE_URL +
+                "/api/ubicacion/ciudades/" +
+                state_id,
+            {
+                method: "GET",
+            }
+        );
+
+        const result = await response.json();
+        res.status(response.status).json(result);
+    }
+}
+
+export const config = {
+    api: {
+        responseLimit: false,
+    },
+};
---
+++ pages/api/ubicacion/estados/[country_id].js
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+/**
+ * Handler para la ruta API de Next.js que maneja la obtención de los estados de un país.
+ *
+ * @param {NextApiRequest} req - El objeto de solicitud HTTP.
+ * @param {NextApiResponse} res - El objeto de respuesta HTTP.
+ *
+ * @returns {Promise<void>} - Una promesa que se resuelve cuando la operación de inicio de sesión se completa.
+ **/
+export default async function handler(req, res) {
+    const country_id = parseInt(req.query.country_id);
+
+    if (isNaN(country_id)) res.status(200).end();
+    else {
+        const response = await fetch(
+            process.env.NEXT_PUBLIC_BASE_URL +
+                "/api/ubicacion/estados/" +
+                country_id,
+            {
+                method: "GET",
+            }
+        );
+
+        const result = await response.json();
+        res.status(response.status).json(result);
+    }
+}
+
+export const config = {
+    api: {
+        responseLimit: false,
+    },
+};
