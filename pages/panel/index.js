1787f67ac42ef88ace18477c6c7a889f52738dbb
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-26 18:05:07 -0500

    Añadir graficos al panel de estadisticas

--- pages/panel/index.js
+++ pages/panel/index.js
@@ -10,8 +10,10 @@ import Typography from "@mui/joy/Typography";
 import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 
-import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Grid2";
+import Stack from "@mui/material/Stack";
+
+import { PieChart, pieArcLabelClasses } from "@mui/x-charts/PieChart";
 
 import Layout from "@/components/Home/Layout";
 
@@ -96,42 +98,50 @@ export default function Panel() {
             </Box>
             <Grid container spacing={1.25}>
                 <Grid size={{ xs: 12, md: 4 }}>
-                    <Card variant="plain">
+                    <Card
+                        variant="plain"
+                        sx={{
+                            width: "100%",
+                            height: "100%",
+                        }}
+                    >
                         <CardContent>
                             <Typography level="title-lg">
                                 Personas registradas
                             </Typography>
-                            <Stack
-                                direction="row"
-                                alignItems="center"
-                                justifyContent="space-between"
-                                spacing={1.25}
-                            >
-                                <Typography level="body-md">Hoy</Typography>
-                                <Typography level="h2">
-                                    {isLoading ? (
-                                        <CircularProgress size="sm" />
-                                    ) : (
-                                        formatNumber(data.today)
-                                    )}
-                                </Typography>
-                            </Stack>
-                            <Stack
-                                direction="row"
-                                alignItems="center"
-                                justifyContent="space-between"
-                                spacing={1.25}
-                            >
-                                <Typography level="body-md">
-                                    Total, desde el inicio
-                                </Typography>
-                                <Typography level="h2">
-                                    {isLoading ? (
-                                        <CircularProgress size="sm" />
-                                    ) : (
-                                        formatNumber(data.total)
-                                    )}
-                                </Typography>
+                            <Stack flex={1} justifyContent="center">
+                                <Stack
+                                    direction="row"
+                                    alignItems="center"
+                                    justifyContent="space-between"
+                                    spacing={1.25}
+                                >
+                                    <Typography level="body-md">Hoy</Typography>
+                                    <Typography level="h2">
+                                        {isLoading ? (
+                                            <CircularProgress size="sm" />
+                                        ) : (
+                                            formatNumber(data.today)
+                                        )}
+                                    </Typography>
+                                </Stack>
+                                <Stack
+                                    direction="row"
+                                    alignItems="center"
+                                    justifyContent="space-between"
+                                    spacing={1.25}
+                                >
+                                    <Typography level="body-md">
+                                        Total, desde el inicio
+                                    </Typography>
+                                    <Typography level="h2">
+                                        {isLoading ? (
+                                            <CircularProgress size="sm" />
+                                        ) : (
+                                            formatNumber(data.total)
+                                        )}
+                                    </Typography>
+                                </Stack>
                             </Stack>
                         </CardContent>
                     </Card>
@@ -182,6 +192,224 @@ export default function Panel() {
                                     </Typography>
                                 </Stack>
                             </Stack>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid size={{ xs: 12, md: 4 }}>
+                    <Card
+                        variant="plain"
+                        sx={{
+                            width: "100%",
+                            height: "100%",
+                        }}
+                    >
+                        <CardContent>
+                            <Typography level="title-lg">
+                                Personas por etnia
+                            </Typography>
+                            {data?.etnia?.map((item, index) => (
+                                <Stack
+                                    key={index}
+                                    direction="row"
+                                    alignItems="center"
+                                    justifyContent="space-between"
+                                    spacing={1.25}
+                                    flex={1}
+                                >
+                                    <Typography level="body-md">
+                                        {item.label}
+                                    </Typography>
+                                    <Typography level="h2">
+                                        {formatNumber(item.value)}
+                                    </Typography>
+                                </Stack>
+                            ))}
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid size={{ xs: 12, md: 4 }}>
+                    <Card
+                        variant="plain"
+                        sx={{
+                            width: "100%",
+                            height: "100%",
+                        }}
+                    >
+                        <CardContent>
+                            <Typography level="title-lg">
+                                Personas por rangos de edad
+                            </Typography>
+                            <PieChart
+                                series={[
+                                    {
+                                        data: data?.edad ?? [],
+                                        // innerRadius: 50,
+                                        // outerRadius: 80,
+                                        paddingAngle: 5,
+                                        cornerRadius: 5,
+                                        // startAngle: -45,
+                                        // endAngle: 225,
+                                        arcLabel: (item) => `${item.value}`,
+                                        arcLabelMinAngle: 35,
+                                        arcLabelRadius: "60%",
+                                    },
+                                ]}
+                                height={150}
+                                sx={{
+                                    [`& .${pieArcLabelClasses.root}`]: {
+                                        fontWeight: "bold",
+                                    },
+                                }}
+                            />
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid size={{ xs: 12, md: 4 }}>
+                    <Card
+                        variant="plain"
+                        sx={{
+                            width: "100%",
+                            height: "100%",
+                        }}
+                    >
+                        <CardContent>
+                            <Typography level="title-lg">
+                                Personas por genero
+                            </Typography>
+                            <PieChart
+                                series={[
+                                    {
+                                        data: data?.genero ?? [],
+                                        // innerRadius: 50,
+                                        // outerRadius: 80,
+                                        paddingAngle: 5,
+                                        cornerRadius: 5,
+                                        // startAngle: -45,
+                                        // endAngle: 225,
+                                        arcLabel: (item) => `${item.value}`,
+                                        arcLabelMinAngle: 35,
+                                        arcLabelRadius: "60%",
+                                    },
+                                ]}
+                                height={150}
+                                sx={{
+                                    [`& .${pieArcLabelClasses.root}`]: {
+                                        fontWeight: "bold",
+                                    },
+                                }}
+                            />
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid size={{ xs: 12, md: 4 }}>
+                    <Card
+                        variant="plain"
+                        sx={{
+                            width: "100%",
+                            height: "100%",
+                        }}
+                    >
+                        <CardContent>
+                            <Typography level="title-lg">
+                                Personas por rol
+                            </Typography>
+                            <PieChart
+                                series={[
+                                    {
+                                        data: data?.rol ?? [],
+                                        // innerRadius: 50,
+                                        // outerRadius: 80,
+                                        paddingAngle: 5,
+                                        cornerRadius: 5,
+                                        // startAngle: -45,
+                                        // endAngle: 225,
+                                        arcLabel: (item) => `${item.value}`,
+                                        arcLabelMinAngle: 35,
+                                        arcLabelRadius: "60%",
+                                    },
+                                ]}
+                                height={150}
+                                sx={{
+                                    [`& .${pieArcLabelClasses.root}`]: {
+                                        fontWeight: "bold",
+                                    },
+                                }}
+                            />
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid size={{ xs: 12, md: 6 }}>
+                    <Card
+                        variant="plain"
+                        sx={{
+                            width: "100%",
+                            height: "100%",
+                        }}
+                    >
+                        <CardContent>
+                            <Typography level="title-lg">
+                                Personas por zona
+                            </Typography>
+                            <PieChart
+                                series={[
+                                    {
+                                        data: data?.zona ?? [],
+                                        // innerRadius: 50,
+                                        // outerRadius: 80,
+                                        paddingAngle: 5,
+                                        cornerRadius: 5,
+                                        // startAngle: -45,
+                                        // endAngle: 225,
+                                        arcLabel: (item) => `${item.value}`,
+                                        arcLabelMinAngle: 35,
+                                        arcLabelRadius: "60%",
+                                    },
+                                ]}
+                                height={150}
+                                sx={{
+                                    [`& .${pieArcLabelClasses.root}`]: {
+                                        fontWeight: "bold",
+                                    },
+                                }}
+                            />
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid size={{ xs: 12, md: 6 }}>
+                    <Card
+                        variant="plain"
+                        sx={{
+                            width: "100%",
+                            height: "100%",
+                        }}
+                    >
+                        <CardContent>
+                            <Typography level="title-lg">
+                                Personas interesadas en continuar con el curso
+                                de 120 horas
+                            </Typography>
+                            <PieChart
+                                series={[
+                                    {
+                                        data: data?.continuar_curso ?? [],
+                                        // innerRadius: 50,
+                                        // outerRadius: 80,
+                                        paddingAngle: 5,
+                                        cornerRadius: 5,
+                                        // startAngle: -45,
+                                        // endAngle: 225,
+                                        arcLabel: (item) => `${item.value}`,
+                                        arcLabelMinAngle: 35,
+                                        arcLabelRadius: "60%",
+                                    },
+                                ]}
+                                height={150}
+                                sx={{
+                                    [`& .${pieArcLabelClasses.root}`]: {
+                                        fontWeight: "bold",
+                                    },
+                                }}
+                            />
                         </CardContent>
                     </Card>
                 </Grid>
