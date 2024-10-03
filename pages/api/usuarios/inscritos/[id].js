7737f21396282c36c057c8c8170c251f423f208f
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-10-03 15:13:12 -0500

    Refactorizar manejo de errores en la API de usuarios inscritos

--- pages/api/usuarios/inscritos/[id].js
+++ pages/api/usuarios/inscritos/[id].js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
             }
         );
         const result = await response.json();
-        res.status(response.status).json(result);
+        res.status(response.status).json(result || response.statusText);
     } else if (req.method === "GET") {
         const response = await fetch(
             process.env.NEXT_PUBLIC_BASE_URL + "/api/usuarios/inscritos/" + id,
