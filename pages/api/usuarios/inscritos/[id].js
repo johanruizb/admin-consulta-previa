fce53089d02ba7b7fc273bd540e53f0a514059d0
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-10-09 14:28:18 -0500

    Refactorizar componente FileField y agregar funcionalidad de reemplazar imagen

--- pages/api/usuarios/inscritos/[id].js
+++ pages/api/usuarios/inscritos/[id].js
@@ -1,8 +1,18 @@
 import { getIronSession } from "iron-session";
 import { NextApiRequest, NextApiResponse } from "next";
 
+// import { Buffer } from "node:buffer";
+
+async function buffer(readable) {
+    const chunks = [];
+    for await (const chunk of readable) {
+        chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+    }
+    return Buffer.concat(chunks);
+}
+
 /**
- * Handler para la ruta API de Next.js que maneja la obtención la información de un inscrito.
+ * Handler para la ruta API de Next.js que maneja la actualización y recuperación de los datos de los usuarios inscritos.
  *
  * @param {NextApiRequest} req - El objeto de solicitud HTTP.
  * @param {NextApiResponse} res - El objeto de respuesta HTTP.
@@ -18,21 +28,45 @@ export default async function handler(req, res) {
     const id = parseInt(req.query.id);
     if (!id) {
         res.status(200).end();
-        return;
     }
 
-    if (req.method === "POST") {
+    // else if (req.method === "POST") {
+    //     console.log("typeof req.body", typeof req.body);
+    //     console.log(
+    //         "req.body instanceof FormData",
+    //         req.body instanceof FormData
+    //     );
+
+    //     const response = await fetch(
+    //         process.env.NEXT_PUBLIC_BASE_URL + "/api/usuarios/inscritos/" + id,
+    //         {
+    //             method: "PUT",
+    //             headers: {
+    //                 Authorization: "Bearer " + session.accessToken,
+    //                 "Content-Type": "multipart/form-data; boundary=----",
+    //             },
+    //             body: req.body,
+    //         }
+    //     );
+    //     const result = await response.json();
+    //     res.status(response.status).json(result || response.statusText);
+    // }
+    else if (req.method === "POST") {
+        const formDataBuffer = await buffer(req);
+
+        // Hacer la solicitud al servidor Django reenviando el FormData
         const response = await fetch(
-            process.env.NEXT_PUBLIC_BASE_URL + "/api/usuarios/inscritos/" + id,
+            `${process.env.NEXT_PUBLIC_BASE_URL}/api/usuarios/inscritos/${id}`,
             {
                 method: "PUT",
                 headers: {
-                    Authorization: "Bearer " + session.accessToken,
-                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${session.accessToken}`,
+                    "Content-Type": "multipart/form-data; boundary=----",
                 },
-                body: JSON.stringify(req.body),
+                body: formDataBuffer,
             }
         );
+
         const result = await response.json();
         res.status(response.status).json(result || response.statusText);
     } else if (req.method === "GET") {
@@ -53,5 +87,6 @@ export default async function handler(req, res) {
 export const config = {
     api: {
         responseLimit: false,
+        bodyParser: false,
     },
 };
