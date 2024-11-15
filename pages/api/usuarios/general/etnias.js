77b5e9713b08563b584bf0b9200824fbae609348
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-11-14 21:19:44 -0500

    Agregar nuevos endpoints de API para conectividad, etnias, géneros y tipo de cliente

---
+++ pages/api/usuarios/general/etnias.js
@@ -0,0 +1,28 @@
+import { getIronSession } from "iron-session";
+
+export default async function handler(req, res) {
+    const session = await getIronSession(req, res, {
+        password: process.env.SESSION_SECRET,
+        cookieName: "session",
+    });
+
+    const response = await fetch(
+        process.env.NEXT_PUBLIC_BASE_URL + "/api/usuarios/etnias",
+        {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + session.accessToken,
+                "X-Referer": req.headers.referer,
+            },
+        }
+    );
+    const result = await response.json();
+    res.status(response.status).json(result);
+}
+
+export const config = {
+    api: {
+        responseLimit: false,
+        bodyParser: false,
+    },
+};
