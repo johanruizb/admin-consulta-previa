b792f36d61f4ba191fc638509ef9dfe92fc47fed
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-11-14 21:34:05 -0500

    Ajustar configuración de API para establecer un límite máximo de duración de respuesta

--- pages/api/moodle/reporte/resumen.js
+++ pages/api/moodle/reporte/resumen.js
@@ -41,6 +41,7 @@ export default async function handler(req, res) {
 export const config = {
     api: {
         responseLimit: false,
+        maxDuration: 30,
         // bodyParser: false,
     },
 };
