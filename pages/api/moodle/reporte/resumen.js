7d67d13f0087700905c675e3e04c32d67678c230
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-11-14 23:47:37 -0500

    Ajustar la duración máxima de respuesta de la API a 60 segundos

--- pages/api/moodle/reporte/resumen.js
+++ pages/api/moodle/reporte/resumen.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
 export const config = {
     api: {
         responseLimit: false,
-        maxDuration: 30,
+        maxDuration: 60,
         // bodyParser: false,
     },
 };
