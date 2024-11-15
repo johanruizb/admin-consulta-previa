f3733a60078f2b3dbca5c586313bca41b32533e2
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-11-14 21:19:18 -0500

    Actualizar formulario de cursos: reemplazar selectores estáticos por AsyncSelect y agregar nuevas opciones para género, etnia, tipo de cliente, zona de residencia y conectividad

--- components/Cursos/constants.js
+++ components/Cursos/constants.js
@@ -25,192 +25,74 @@ const FormularioCursos = [
         },
         url: "/api/ubicacion/estados/48",
     },
-    // {
-    //     Component: CustomSelect,
-    //     controller: {
-    //         name: "genero",
-    //         defaultValue: "",
-    //     },
-    //     field: {
-    //         options: [
-    //             {
-    //                 value: 1,
-    //                 label: "Femenino",
-    //             },
-    //             {
-    //                 value: 2,
-    //                 label: "Masculino",
-    //             },
-    //             {
-    //                 value: 3,
-    //                 label: "Transgénero",
-    //             },
-    //             {
-    //                 value: 4,
-    //                 label: "No binario",
-    //             },
-    //             {
-    //                 value: 5,
-    //                 label: "Prefiero no decirlo",
-    //             },
-    //             {
-    //                 value: 0,
-    //                 label: "Otro (especifique)",
-    //             },
-    //         ],
-    //         label: "Género",
-    //         placeholder: "Seleccione un género",
-    //     },
-    // },
-    // {
-    //     Component: CustomSelect,
-    //     controller: {
-    //         name: "etnia",
-    //         defaultValue: "",
-    //     },
-    //     field: {
-    //         options: [
-    //             {
-    //                 value: 1,
-    //                 label: "Indígena",
-    //             },
-    //             {
-    //                 value: 2,
-    //                 label: "Mestizo",
-    //             },
-    //             {
-    //                 value: 3,
-    //                 label: "Blanco",
-    //             },
-    //             { value: 4, label: "Rom" },
-    //             {
-    //                 value: 5,
-    //                 label: "Raizal del Archipiélago de San Andrés y Providencia",
-    //             },
-    //             {
-    //                 value: 6,
-    //                 label: "Palenquero de San Basilio",
-    //             },
-    //             {
-    //                 value: 7,
-    //                 label: "Negro(a), afrocolombiano(a) o afrodescendiente",
-    //             },
-    //             {
-    //                 value: 8,
-    //                 label: "Mulato(a)",
-    //             },
-    //             {
-    //                 value: 9,
-    //                 label: "Ninguno de los anteriores",
-    //             },
-    //         ],
-    //         label: "Grupo poblacional",
-    //         placeholder: "Seleccione un grupo poblacional",
-    //     },
-    // },
-    // {
-    //     Component: CustomSelect,
-    //     controller: {
-    //         name: "tipo_cliente",
-    //         defaultValue: "",
-    //     },
-    //     field: {
-    //         options: [
-    //             {
-    //                 value: 1,
-    //                 label: "Comunidad Negro(a) afrocolombiano(a) o afrodescendiente",
-    //             },
-    //             {
-    //                 value: 2,
-    //                 label: "Comunidad Indígena",
-    //             },
-    //             {
-    //                 value: 3,
-    //                 label: "Ejecutores de procesos Consulta Previa",
-    //             },
-    //             {
-    //                 value: 4,
-    //                 label: "Institucionalidad interviniente en Consulta Previa",
-    //             },
-    //             {
-    //                 value: 5,
-    //                 label: "Contratista del Ministerio del Interior",
-    //             },
-    //             {
-    //                 value: 6,
-    //                 label: "Funcionarios Ministerio del Interior",
-    //             },
-    //             {
-    //                 value: 7,
-    //                 label: "Población civil",
-    //             },
-    //             {
-    //                 value: 8,
-    //                 label: "Empresario",
-    //             },
-    //             {
-    //                 value: 9,
-    //                 label: "Educación",
-    //             },
-    //         ],
-    //         label: "De los siguientes roles en cuál se reconoce?",
-    //         placeholder: "Seleccione un rol",
-    //     },
-    // },
-    // {
-    //     Component: CustomSelect,
-    //     controller: {
-    //         name: "zona",
-    //         defaultValue: "",
-    //     },
-    //     field: {
-    //         options: [
-    //             {
-    //                 value: "rural",
-    //                 label: "Rural",
-    //             },
-    //             {
-    //                 value: "urbana",
-    //                 label: "Urbana",
-    //             },
-    //         ],
-    //         label: "Zona de residencia",
-    //         placeholder: "Seleccione una zona",
-    //     },
-    // },
-    // {
-    //     Component: CustomSelect,
-    //     controller: {
-    //         name: "conectividad",
-    //         defaultValue: "",
-    //     },
-    //     field: {
-    //         options: [
-    //             {
-    //                 value: "nula",
-    //                 label: "Sin conexión",
-    //             },
-    //             {
-    //                 value: "baja",
-    //                 label: "Solo con wifi público",
-    //             },
-    //             {
-    //                 value: "media",
-    //                 label: "Por intervalos de tiempo con dificultad",
-    //             },
-    //             {
-    //                 value: "plena",
-    //                 label: "Todo el día sin dificultad",
-    //             },
-    //             {
-    //                 value: "otra",
-    //                 label: "Otra (especificar)",
-    //             },
-    //         ],
-    //         label: "Conectividad",
-    //         placeholder: "Seleccione una opción",
-    //     },
-    // },
+    {
+        Component: AsyncSelect,
+        controller: {
+            name: "user__genero_id",
+        },
+        field: {
+            label: "Género",
+            placeholder: "Seleccione un género",
+        },
+        url: "/api/usuarios/general/generos",
+    },
+    {
+        Component: AsyncSelect,
+        controller: {
+            name: "user__etnia",
+        },
+        field: {
+            label: "Grupo poblacional",
+            placeholder: "Seleccione un grupo poblacional",
+        },
+        url: "/api/usuarios/general/etnias",
+    },
+    {
+        Component: AsyncSelect,
+        controller: {
+            name: "user__tipo_cliente",
+        },
+        field: {
+            label: "De los siguientes roles en cuál se reconoce?",
+            placeholder: "Seleccione un rol",
+        },
+        url: "/api/usuarios/general/tipo-cliente",
+    },
+    {
+        Component: CustomSelect,
+        controller: {
+            name: "user__zona",
+        },
+        field: {
+            options: [
+                {
+                    value: "all",
+                    label: "Todas las zonas",
+                },
+                {
+                    value: "rural",
+                    label: "Rural",
+                },
+                {
+                    value: "urbana",
+                    label: "Urbana",
+                },
+            ],
+            label: "Zona de residencia",
+            placeholder: "Seleccione una zona",
+        },
+    },
+    {
+        Component: AsyncSelect,
+        controller: {
+            name: "user__conectividad",
+        },
+        field: {
+            label: "Conectividad",
+            placeholder: "Seleccione una opción",
+        },
+        url: "/api/usuarios/general/conectividad",
+    },
     {
         Component: CustomSelect,
         controller: {
@@ -219,6 +101,10 @@ const FormularioCursos = [
         },
         field: {
             options: [
+                {
+                    value: "all",
+                    label: "Todos los módulos",
+                },
                 {
                     value: 2,
                     label: "Módulo 1 - Derecho Fundamental a la Consulta Previa",
