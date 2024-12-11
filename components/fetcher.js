15b61558777a866767333aeee4ea94dad5947671
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-12-10 19:33:32 -0500

    Mejorar formato de rechazo en el fetcher: agregar sangrías para mayor claridad

--- components/fetcher.js
+++ components/fetcher.js
@@ -2,5 +2,8 @@ export default async function fetcher(...args) {
     const res = await fetch(...args);
     return res.ok
         ? res.json()
-        : Promise.reject({ status: res.status, statusText: res.statusText });
+        : Promise.reject({
+              status: res.status,
+              statusText: res.statusText,
+          });
 }
