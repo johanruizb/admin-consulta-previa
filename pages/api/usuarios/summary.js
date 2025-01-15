56b303cacf5ec08f267855d181112dc770c00073
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2025-01-14 20:40:06 -0500

    Agregar handler para la ruta API que maneja la obtención de usuarios inscritos

---
+++ pages/api/usuarios/summary.js
@@ -0,0 +1,36 @@
+import { getIronSession } from "iron-session";
+import { NextApiRequest, NextApiResponse } from "next";
+
+/**
+ * Handler para la ruta API de Next.js que maneja la obtención de los inscritos.
+ *
+ * @param {NextApiRequest} req - El objeto de solicitud HTTP.
+ * @param {NextApiResponse} res - El objeto de respuesta HTTP.
+ *
+ * @returns {Promise<void>} - Una promesa que se resuelve cuando la operación de inicio de sesión se completa.
+ **/
+export default async function handler(req, res) {
+    const session = await getIronSession(req, res, {
+        password: process.env.SESSION_SECRET,
+        cookieName: "session",
+    });
+
+    const response = await fetch(
+        process.env.NEXT_PUBLIC_BASE_URL + "/api/usuarios/summary",
+        {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + session.accessToken,
+            },
+        }
+    );
+
+    const result = await response.json();
+    res.status(response.status).json(result);
+}
+
+export const config = {
+    api: {
+        responseLimit: false,
+    },
+};
