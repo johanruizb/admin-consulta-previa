a27990b32a0980f69f102dd2d170a8955a97c991
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2025-01-15 18:48:12 -0500

    Comentar el código de la API de estadísticas de usuarios registrados para facilitar la revisión

--- pages/api/usuarios/estadisticas/registrados.js
+++ pages/api/usuarios/estadisticas/registrados.js
@@ -1,38 +1,38 @@
-import { getIronSession } from "iron-session";
-import { NextApiRequest, NextApiResponse } from "next";
+// import { getIronSession } from "iron-session";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-/**
- * Handler para la ruta API de Next.js que maneja la obtención de las estadísticas.
- *
- * @param {NextApiRequest} req - El objeto de solicitud HTTP.
- * @param {NextApiResponse} res - El objeto de respuesta HTTP.
- *
- * @returns {Promise<void>} - Una promesa que se resuelve cuando la operación de inicio de sesión se completa.
- **/
-export default async function handler(req, res) {
-    const session = await getIronSession(req, res, {
-        password: process.env.SESSION_SECRET,
-        cookieName: "session",
-    });
+// /**
+//  * Handler para la ruta API de Next.js que maneja la obtención de las estadísticas.
+//  *
+//  * @param {NextApiRequest} req - El objeto de solicitud HTTP.
+//  * @param {NextApiResponse} res - El objeto de respuesta HTTP.
+//  *
+//  * @returns {Promise<void>} - Una promesa que se resuelve cuando la operación de inicio de sesión se completa.
+//  **/
+// export default async function handler(req, res) {
+//     const session = await getIronSession(req, res, {
+//         password: process.env.SESSION_SECRET,
+//         cookieName: "session",
+//     });
 
-    const response = await fetch(
-        process.env.NEXT_PUBLIC_BASE_URL +
-            "/api/usuarios/estadisticas/registrados",
-        {
-            method: "POST",
-            headers: {
-                Authorization: "Bearer " + session.accessToken,
-                "Content-Type": "application/json",
-            },
-            body: req.body,
-        }
-    );
-    try {
-        const result = await response.json();
-        res.status(response.status).json(result);
-    } catch (error) {
-        res.status(response.status).json({
-            message: `${String(error)} (${response.status})`,
-        });
-    }
-}
+//     const response = await fetch(
+//         process.env.NEXT_PUBLIC_BASE_URL +
+//             "/api/usuarios/estadisticas/registrados",
+//         {
+//             method: "POST",
+//             headers: {
+//                 Authorization: "Bearer " + session.accessToken,
+//                 "Content-Type": "application/json",
+//             },
+//             body: req.body,
+//         }
+//     );
+//     try {
+//         const result = await response.json();
+//         res.status(response.status).json(result);
+//     } catch (error) {
+//         res.status(response.status).json({
+//             message: `${String(error)} (${response.status})`,
+//         });
+//     }
+// }
