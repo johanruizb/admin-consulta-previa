e5c4a7e3189f965be64fada11316d57f45b80a58
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2025-02-27 14:58:40 -0500

    Refactor middleware para simplificar la lógica de rutas públicas y protegidas; mejorar la claridad del código

--- middleware.js
+++ middleware.js
@@ -2,16 +2,13 @@ import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-// 1. Specify protected and public routes
 const publicRoutes = ["/login"];
 
 export default async function middleware(req) {
-    // 2. Check if the current route is protected or public
     const path = req.nextUrl.pathname;
     const isPublicRoute = publicRoutes.includes(path);
     const isProtectedRoute = !isPublicRoute;
 
-    // 3. Decrypt the session from the cookie
     const userCookies = await cookies();
 
     const session = await getIronSession(userCookies, {
@@ -19,22 +16,15 @@ export default async function middleware(req) {
         cookieName: "session",
     });
 
-    // 5. Redirect to /login if the user is not authenticated
     if (isProtectedRoute && !session?.accessToken)
         return NextResponse.redirect(new URL("/login", req.nextUrl));
 
-    // 6. Redirect to / if the user is authenticated
-    if (
-        isPublicRoute &&
-        session?.accessToken
-        // && !path.startsWith("/")
-    )
+    if (isPublicRoute && session?.accessToken)
         return NextResponse.redirect(new URL("/", req.nextUrl));
 
     return NextResponse.next();
 }
 
-// Routes Middleware should not run on
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
