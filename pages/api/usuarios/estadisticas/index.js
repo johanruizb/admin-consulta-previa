620206bc6fbb4a47a42c1129be9947c806621fd9
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2025-10-03 18:13:39 -0500

    feat: agregar parámetros de consulta en el handler de estadísticas y eliminar archivo obsoleto

--- pages/api/usuarios/estadisticas/index.js
+++ pages/api/usuarios/estadisticas/index.js
@@ -14,9 +14,12 @@ export default async function handler(req, res) {
         password: process.env.SESSION_SECRET,
         cookieName: "session",
     });
-
+    
+    const queryParams = new URLSearchParams(req.query).toString();
     const response = await fetch(
-        process.env.NEXT_PUBLIC_BASE_URL + "/api/usuarios/estadisticas",
+        process.env.NEXT_PUBLIC_BASE_URL +
+            "/api/usuarios/estadisticas?" +
+            queryParams,
         {
             method: "POST",
             headers: {
@@ -24,7 +27,7 @@ export default async function handler(req, res) {
                 "Content-Type": "application/json",
             },
             body: req.body,
-        },
+        }
     );
 
     if (response.ok) {
