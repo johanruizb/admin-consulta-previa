fb0455217d49982158e00312372e56c4c72f967a
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2025-10-06 20:09:26 -0500

    feat: agregar handler para la API de inscripciones por período

---
+++ pages/api/usuarios/inscripciones-por-periodo.js
@@ -0,0 +1,40 @@
+import { getIronSession } from "iron-session";
+import { NextApiRequest, NextApiResponse } from "next";
+
+/**
+ * Handler para la ruta API de Next.js que maneja la obtención de inscripciones por período.
+ *
+ * @param {NextApiRequest} req - El objeto de solicitud HTTP.
+ * @param {NextApiResponse} res - El objeto de respuesta HTTP.
+ *
+ * @returns {Promise<void>} - Una promesa que se resuelve cuando la operación se completa.
+ **/
+export default async function handler(req, res) {
+    const session = await getIronSession(req, res, {
+        password: process.env.SESSION_SECRET,
+        cookieName: "session",
+    });
+
+    // Construir la URL con query parameters
+    const queryParams = new URLSearchParams(req.query).toString();
+    const url =
+        process.env.NEXT_PUBLIC_BASE_URL +
+        "/api/usuarios/inscripciones-por-periodo" +
+        (queryParams ? `?${queryParams}` : "");
+
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            Authorization: "Bearer " + session.accessToken,
+        },
+    });
+
+    const result = await response.json();
+    res.status(response.status).json(result);
+}
+
+export const config = {
+    api: {
+        responseLimit: false,
+    },
+};
