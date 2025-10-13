5de915407aa8eb04726f362bd093f59806c80c59
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2025-10-13 17:08:56 -0500

    feat: integrar ClerkProvider en la aplicación y mejorar la estructura del documento

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import CustomAlert from "@/components/Alert";
 import AppInitializer from "@/components/AppInitializer";
 import PermissionProvider from "@/components/Home/permissionContext/PermissionProvider";
+import Navigate from "@/components/Navigate";
 import { CicloProvider } from "@/contexts/CicloContext";
 import "@/styles/Avances.css";
 import "@/styles/globals.css";
 import "@/styles/Option.css";
 import "@/styles/OrderTable.css";
+import { ClerkProvider, SignedOut } from "@clerk/nextjs";
 import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
 import CssBaseline from "@mui/joy/CssBaseline";
@@ -42,40 +44,47 @@ export default function App({ Component, pageProps }) {
                     defaultMode="light"
                 >
                     <CssBaseline enableColorScheme />
-                    <SWRConfig
-                        value={{
-                            revalidateOnMount: true,
-                            fetcher: async (...args) => {
-                                const res = await fetch(...args);
-                                return res.ok
-                                    ? res.json()
-                                    : Promise.reject({
-                                          status: res.status,
-                                          statusText: res.statusText,
-                                      });
-                            },
+                    <ClerkProvider
+                        {...pageProps}
+                        appearance={{
+                            cssLayerName: "clerk",
                         }}
                     >
-                        <SnackbarProvider
-                            maxSnack={3}
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "right",
+                        <SWRConfig
+                            value={{
+                                revalidateOnMount: true,
+                                fetcher: async (...args) => {
+                                    const res = await fetch(...args);
+                                    return res.ok
+                                        ? res.json()
+                                        : Promise.reject({
+                                              status: res.status,
+                                              statusText: res.statusText,
+                                          });
+                                },
                             }}
-                            autoHideDuration={5000}
                         >
-                            <PermissionProvider>
-                                <AppInitializer>
-                                    <CicloProvider>
-                                        <Fragment>
-                                            <Component {...pageProps} />
-                                            <CustomAlert />
-                                        </Fragment>
-                                    </CicloProvider>
-                                </AppInitializer>
-                            </PermissionProvider>
-                        </SnackbarProvider>
-                    </SWRConfig>
+                            <SnackbarProvider
+                                maxSnack={3}
+                                anchorOrigin={{
+                                    vertical: "bottom",
+                                    horizontal: "right",
+                                }}
+                                autoHideDuration={5000}
+                            >
+                                <PermissionProvider>
+                                    <AppInitializer>
+                                        <CicloProvider>
+                                            <Fragment>
+                                                <Component {...pageProps} />
+                                                <CustomAlert />
+                                            </Fragment>
+                                        </CicloProvider>
+                                    </AppInitializer>
+                                </PermissionProvider>
+                            </SnackbarProvider>
+                        </SWRConfig>
+                    </ClerkProvider>
                 </ThemeProvider>
             </JoyCssVarsProvider>
         </CacheProvider>
