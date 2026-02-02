433a750fbb94d01ddffe728437d323aeaf7fdcad
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2026-02-02 16:46:35 -0500

    feat: agregar propiedad isAdmin al contexto de permisos y optimizar la lógica de permisos

--- components/Home/permissionContext/index.js
+++ components/Home/permissionContext/index.js
@@ -3,6 +3,7 @@ const { createContext } = require("react");
 const PermissionContext = createContext({
     permissions: null,
     hasPermission: () => false,
+    isAdmin: false,
     isLoading: true,
     isValidating: false,
     error: null,
